cff5fa89b3dd563ec016addc105eaa74ca9f5d2f
Michael Doron <michael.doron@gmail.com>
2019-10-24 14:34:42 +0300

    Revert "Appendix changes"
    This reverts commit 0fcc452c6febf5fb0fde0ee2c1372942f70c004e.

--- agent.py
+++ agent.py
@@ -1,63 +1,32 @@
 import os
-import numpy as np
-import torch
-from tensorboardX import SummaryWriter
-from torch import optim
-from model import DQN
-import matplotlib
-matplotlib.use('agg')
-import numpy as np
-import matplotlib.pyplot as plt
-from torch import nn
 import random
-import numpy as np
+import torch
+from torch import optim
+
+from model import DQN
+
 
 class Agent():
   def __init__(self, args, env):
     self.action_space = env.action_space()
-    self.args = args
     self.atoms = args.atoms
     self.Vmin = args.V_min
     self.Vmax = args.V_max
-    self.original_loss = torch.Tensor([0.0]).to(device=args.device)
-    self.original_raw_loss = torch.Tensor([0.0]).to(device=args.device)
     self.support = torch.linspace(args.V_min, args.V_max, self.atoms).to(device=args.device)  # Support (range) of z
     self.delta_z = (args.V_max - args.V_min) / (self.atoms - 1)
     self.batch_size = args.batch_size
     self.n = args.multi_step
     self.discount = args.discount
-    self.losses = []
-    self.device = args.device
-    self.parameters = {}
-    self.zeroed_weights = []
-    self.regularization_norms = []
-    self.raw_losses = []
-    self.regularization_weight = torch.Tensor([0.]).to(device=args.device)  
-    self.regularization_norm = 1
+
     self.online_net = DQN(args, self.action_space).to(device=args.device)
-    self.target_net = DQN(args, self.action_space).to(device=args.device)
-    if (args.compress and (args.evaluate)):
-      print('added compression for evaluation')
-      self.target_net.add_regularization_norm(args)
-      self.online_net.add_regularization_norm(args)
-    if (args.add_appendix and (args.evaluate or args.compress == False)):
-      print('added appendix')
-      self.add_appendix(args, args.type_of_norm)
-      if (args.just_appendix):
-        self.online_net.forward = self.online_net.forward_appendix_just_appendix
     if args.model and os.path.isfile(args.model):
       # Always load tensors onto CPU by default, will shift to GPU if necessary
-      self.online_net.load_state_dict(torch.load(args.model, map_location='cpu'), strict=False)
-
+      self.online_net.load_state_dict(torch.load(args.model, map_location='cpu'))
     self.online_net.train()
 
+    self.target_net = DQN(args, self.action_space).to(device=args.device)
     self.update_target_net()
     self.target_net.train()
-
-    if (args.compress and not (args.evaluate)):
-      self.target_net.add_regularization_norm(args)
-      self.online_net.add_regularization_norm(args)
-
     for param in self.target_net.parameters():
       param.requires_grad = False
 
@@ -76,7 +45,7 @@ class Agent():
   def act_e_greedy(self, state, epsilon=0.001):  # High ε can reduce evaluation scores drastically
     return random.randrange(self.action_space) if random.random() < epsilon else self.act(state)
 
-  def learn(self, mem, add_appendix, compress, started_compressing, type_of_norm, summary_writer, T):
+  def learn(self, mem):
     # Sample transitions
     idxs, states, actions, returns, next_states, nonterminals, weights = mem.sample(self.batch_size)
 
@@ -108,151 +77,28 @@ class Agent():
       offset = torch.linspace(0, ((self.batch_size - 1) * self.atoms), self.batch_size).unsqueeze(1).expand(self.batch_size, self.atoms).to(actions)
       m.view(-1).index_add_(0, (l + offset).view(-1), (pns_a * (u.float() - b)).view(-1))  # m_l = m_l + p(s_t+n, a*)(u - b)
       m.view(-1).index_add_(0, (u + offset).view(-1), (pns_a * (b - l.float())).view(-1))  # m_u = m_u + p(s_t+n, a*)(b - l)
-    
+
     loss = -torch.sum(m * log_ps_a, 1)  # Cross-entropy loss (minimises DKL(m||p(s_t, a_t)))
-    
-    self.losses.append((weights * loss).mean().mean().detach().cpu().numpy())
-    self.raw_losses.append(loss.mean().mean().detach().cpu().numpy())
-    if self.original_loss.data == 0: self.original_loss = torch.mean(torch.Tensor(np.array(self.losses)).to(device=self.device)).to(device=self.device)
-    if self.original_raw_loss.data == 0: self.original_raw_loss = torch.mean(torch.Tensor(np.array(self.raw_losses)).to(device=self.device)).to(device=self.device)
-    
-    if (compress and started_compressing): # Add L0 norm for sparsity
-      self.set_regularization_weight(print_weights = add_appendix, type_of_norm = type_of_norm)
     self.online_net.zero_grad()
-
-    if (compress and started_compressing) or (self.args.add_appendix and (self.args.compress == False)): # Add L0 norm for sparsity
-      final_loss = (weights * loss)
-      final_loss += self.get_weighted_regularization_norm(type_of_norm)
-      summary_writer.add_scalar('loss', (self.losses[-1]), T)
-      summary_writer.add_scalar('raw loss', (self.raw_losses[-1]), T)
-      summary_writer.add_scalar('regularization loss', (self.get_regularization_norm(type_of_norm)).detach().cpu().numpy(), T)
-      summary_writer.add_scalar('weighted regularization loss', (self.get_weighted_regularization_norm(type_of_norm)).detach().cpu().numpy(), T)
-      summary_writer.add_scalar('regularization weight', self.regularization_weight, T)
-      (final_loss).mean().backward()  # Backpropagate importance-weighted minibatch loss
-    if (add_appendix):
-      summary_writer.add_scalar('Merger weight', (self.online_net.app4.sigmoid_center).detach().cpu().numpy().mean(), T)
-      summary_writer.add_scalar('linear weight', (self.online_net.app_lin.weight).detach().cpu().numpy().mean(), T)
-      if self.args.freeze_merge == False:
-        summary_writer.add_scalar('Merger grad', (self.online_net.app4.sigmoid_center.grad.mean()).detach().cpu().numpy(), T)
-
-      if type_of_norm == 'L0':
-        self.zeroed_weights.append(self.get_regularization_percentage())
-      self.regularization_norms.append(self.get_regularization_norm(type_of_norm))
-
-    else:
-      ((weights * loss)).mean().backward()  # Backpropagate importance-weighted minibatch loss
-      summary_writer.add_scalar('loss', (self.losses[-1]), T)
-      summary_writer.add_scalar('raw loss', (self.raw_losses[-1]), T)
+    (weights * loss).mean().backward()  # Backpropagate importance-weighted minibatch loss
     self.optimiser.step()
-    if (add_appendix): # Add L0 norm for sparsity
-      if self.args.merge_type == 'addition':
-        self.online_net.app4.sigmoid_center.data.clamp_(min=0.0, max=1.0)
-      else:
-        self.online_net.app4.sigmoid_center.data.clamp_(min=-0.05, max=1.05)
 
     mem.update_priorities(idxs, loss.detach().cpu().numpy())  # Update priorities of sampled transitions
-    if (compress and started_compressing):
-      return -torch.sum(m * log_ps_a, 1), self.get_regularization_norm(type_of_norm), self.get_weighted_regularization_norm(type_of_norm)
-    return loss
+
   def update_target_net(self):
     self.target_net.load_state_dict(self.online_net.state_dict())
-  
+
   # Save model parameters on current device (don't move model between devices)
-  def save(self, path, specific_title, args):
-    torch.save(self.online_net.state_dict(), os.path.join(path, self.args.base_folder + 'models/' + specific_title + '_' + 'model.pth'))
+  def save(self, path):
+    torch.save(self.online_net.state_dict(), os.path.join(path, 'model.pth'))
 
   # Evaluates Q-value based on single state (no batch)
   def evaluate_q(self, state):
     with torch.no_grad():
       return (self.online_net(state.unsqueeze(0)) * self.support).sum(2).max(1)[0].item()
-      
+
   def train(self):
     self.online_net.train()
 
   def eval(self):
     self.online_net.eval()
-
-  def weight_increase(self, losses, raw_losses, smoothness, max_value, regularization_weight):
-    result_raw = ((self.original_raw_loss - raw_losses[-10:].mean()).clamp_(max=0) * smoothness).exp()
-    result = ((self.original_loss - losses[-10:].mean()).clamp_(max=0) * smoothness).exp()
-    return ((min(result, result_raw) * max_value * 10) - (9 * max_value))
-
-  def set_regularization_weight(self, print_weights, type_of_norm):
-    if len(self.losses) <= 1000: 
-      if (type_of_norm == 'L0'):
-        self.online_net.conv1.lamba = self.regularization_weight
-        self.online_net.conv2.lamba = self.regularization_weight
-        self.online_net.conv3.lamba = self.regularization_weight
-      return self.regularization_weight
-    self.regularization_weight += self.weight_increase(torch.Tensor(np.array(self.losses)).to(device=self.device),torch.Tensor(np.array(self.raw_losses)).to(device=self.device), smoothness = 50, max_value = 0.0001, regularization_weight = self.regularization_weight).to(device=self.device)
-    self.regularization_weight.clamp_(min=0)
-
-    if print_weights:
-      print('self.original_loss {}'.format(self.original_loss.item()))
-      print('self.original_raw_loss {}'.format(self.original_raw_loss.item()))
-      print('Merger appendix weights: {}'.format((self.online_net.app4.sigmoid_center).detach().cpu().numpy().mean()))
-      print('Non merger appendix weights: {}'.format((1 - self.online_net.app4.sigmoid_center).detach().cpu().numpy().mean()))
-        
-    if (type_of_norm == 'L0'):
-      self.online_net.conv1.lamba = self.regularization_weight
-      self.online_net.conv2.lamba = self.regularization_weight
-      self.online_net.conv3.lamba = self.regularization_weight
-    return self.regularization_weight
-
-  def get_regularization_percentage(self):
-    result = 0
-    result += (float((self.online_net.conv1.sample_weights() == 0).sum()))
-    result += (float((self.online_net.conv2.sample_weights() == 0).sum()))
-    result += (float((self.online_net.conv3.sample_weights() == 0).sum()))
-    result /=  (float((self.online_net.conv1.sample_weights()).numel()) + float((self.online_net.conv2.sample_weights()).numel()) + float((self.online_net.conv3.sample_weights()).numel()))
-    return result    
-
-  def get_regularization_norm(self, type_of_norm):
-    if (type_of_norm == 'L0'):
-      self.online_net.conv1.lamba = 1
-      self.online_net.conv2.lamba = 1
-      self.online_net.conv3.lamba = 1
-      self.regularization_norm = - (1. / 50000) * self.online_net.conv1.regularization()
-      self.regularization_norm += - (1. / 50000) * self.online_net.conv2.regularization()
-      self.regularization_norm += - (1. / 50000) * self.online_net.conv3.regularization()
-      self.online_net.conv1.lamba = self.regularization_weight
-      self.online_net.conv2.lamba = self.regularization_weight
-      self.online_net.conv3.lamba = self.regularization_weight    
-    else:
-      self.regularization_norm =  self.online_net.conv1.weight.abs().sum()
-      self.regularization_norm += self.online_net.conv2.weight.abs().sum()
-      self.regularization_norm += self.online_net.conv3.weight.abs().sum()
-
-    return self.regularization_norm
-
-  def get_weighted_regularization_norm(self, type_of_norm):
-    if (type_of_norm == 'L0'):
-      self.regularization_norm = - (1. / 50000) * self.online_net.conv1.regularization()
-      self.regularization_norm += - (1. / 50000) * self.online_net.conv2.regularization()
-      self.regularization_norm += - (1. / 50000) * self.online_net.conv3.regularization()
-    else:
-      self.regularization_norm =  self.online_net.conv1.weight.abs().sum()
-      self.regularization_norm += self.online_net.conv2.weight.abs().sum()
-      self.regularization_norm += self.online_net.conv3.weight.abs().sum()
-    return self.regularization_weight * self.regularization_norm    
-
-  def get_number_of_zero_elements(self):
-    number_of_zero_elements = 0
-    print('zero elements in conv1 {}, conv2 {}, conv3 {}'.format(
-      len([p for p in self.online_net.conv1.sample_z(1, False).flatten().detach().cpu().numpy() if p == 0]),
-      len([p for p in self.online_net.conv2.sample_z(1, False).flatten().detach().cpu().numpy() if p == 0]),
-      len([p for p in self.online_net.conv3.sample_z(1, False).flatten().detach().cpu().numpy() if p == 0])))
-    return number_of_zero_elements
-
-  def add_appendix(self, args, type_of_norm):
-    self.online_net.add_appendix(args.device, args.appendix_type, type_of_norm, args.multiplier)
-    self.online_net.to(device=args.device)
-    self.target_net.add_appendix(args.device, args.appendix_type, type_of_norm, args.multiplier)
-    self.target_net.to(device=args.device)
-    self.optimiser = optim.Adam(self.online_net.parameters(), lr=args.lr, eps=args.adam_eps)
-    if type_of_norm == 'L0':
-      with torch.no_grad(): self.original_regularization_norm = self.online_net.conv1.regularization().abs()
-    if (self.args.just_appendix):
-      self.online_net.forward = self.online_net.forward_appendix_just_appendix
-      self.target_net.forward = self.target_net.forward_appendix_just_appendix
-
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import argparse
 from datetime import datetime
-from tensorboardX import SummaryWriter
 import random
 import torch
-import os
+
 from agent import Agent
 from env import Env
 from memory import ReplayMemory
 from test import test
-from tqdm import tqdm
-import copy
+
 
 parser = argparse.ArgumentParser(description='Rainbow')
 parser.add_argument('--seed', type=int, default=123, help='Random seed')
@@ -37,25 +35,13 @@ parser.add_argument('--adam-eps', type=float, default=1.5e-4, metavar='ε', help
 parser.add_argument('--batch-size', type=int, default=32, metavar='SIZE', help='Batch size')
 parser.add_argument('--learn-start', type=int, default=int(80e3), metavar='STEPS', help='Number of steps before starting training')
 parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
-parser.add_argument('--evaluation-interval', type=int, default=10000, metavar='STEPS', help='Number of training steps between evaluations')
-parser.add_argument('--evaluation-episodes', type=int, default=3, metavar='N', help='Number of evaluation episodes to average over')
+parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
+parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
 parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
 parser.add_argument('--log-interval', type=int, default=25000, metavar='STEPS', help='Number of training steps between logging status')
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
-parser.add_argument('--add_appendix', action='store_true', help='Add appendix')
-parser.add_argument('--compress', action='store_true', help='Compress model')
-parser.add_argument('--title', type=str, default='default', help='title of the run')
-parser.add_argument('--appendix_type', type=str, default='', help='type of appendix to add')
-parser.add_argument('--gpu', type=int, default=0, help='gpu_id')
-parser.add_argument('--type_of_norm', type=str, default='L0', help='L0 or L1')
-parser.add_argument('--multiplier', type=float, default=-0.01, help='multiplier')
-parser.add_argument('--appendix_model', type=str, metavar='PARAMS', help='Pretrained model (state dict) for appendix')
-parser.add_argument('--freeze_merge', action='store_true', help='Freeze merge layer')
-parser.add_argument('--merge_type', type=str, default='addition', metavar='PARAMS', help='type of appendix')
-parser.add_argument('--just_appendix', action='store_true', help='remove original network')
-parser.add_argument('--base_folder', type=str, default='/storage/michaeld/', metavar='PARAMS', help='base folder for files')
 
-from torchvision import models, transforms
+
 # Setup
 args = parser.parse_args()
 print(' ' * 26 + 'Options')
@@ -64,7 +50,7 @@ for k, v in vars(args).items():
 random.seed(args.seed)
 torch.manual_seed(random.randint(1, 10000))
 if torch.cuda.is_available() and not args.disable_cuda:
-  args.device = torch.device('cuda', args.gpu)
+  args.device = torch.device('cuda')
   torch.cuda.manual_seed(random.randint(1, 10000))
   torch.backends.cudnn.enabled = False  # Disable nondeterministic ops (not sure if critical but better safe than sorry)
 else:
@@ -84,14 +70,9 @@ action_space = env.action_space()
 
 # Agent
 dqn = Agent(args, env)
-# print(dqn.online_net.conv1.weight)
-
 mem = ReplayMemory(args, args.memory_capacity)
 priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
 
-current_time = str(datetime.now().timestamp())
-log_dir = args.base_folder + 'logs/' + current_time
-summary_writer = SummaryWriter(log_dir='{}/tensorboard/{}_{}/'.format(args.base_folder, args.title, datetime.now().strftime('%Y-%m-%d_%H:%M:%S')))
 
 # Construct validation memory
 val_mem = ReplayMemory(args, args.evaluation_size)
@@ -99,6 +80,7 @@ T, done = 0, True
 while T < args.evaluation_size:
   if done:
     state, done = env.reset(), False
+
   next_state, _, done = env.step(random.randint(0, action_space - 1))
   val_mem.append(state, None, None, done)
   state = next_state
@@ -106,13 +88,13 @@ while T < args.evaluation_size:
 
 if args.evaluate:
   dqn.eval()  # Set DQN (online network) to evaluation mode
-  avg_reward, avg_Q = test(args, 0, dqn, val_mem, args.title, evaluate=True)  # Test
+  avg_reward, avg_Q = test(args, 0, dqn, val_mem, evaluate=True)  # Test
   print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
 else:
   # Training loop
   dqn.train()
   T, done = 0, True
-  for T in tqdm(range(args.T_max)):
+  while T < args.T_max:
     if done:
       state, done = env.reset(), False
     
@@ -126,30 +108,19 @@ else:
     mem.append(state, action, reward, done)  # Append transition to memory
     T += 1
 
-    if T == args.learn_start:
-      if args.add_appendix:
-        dqn.add_appendix(args, args.type_of_norm)
+    if T % args.log_interval == 0:
+      log('T = ' + str(T) + ' / ' + str(args.T_max))
+
+    # Train and test
     if T >= args.learn_start:
       mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
 
       if T % args.replay_frequency == 0:
-        if (args.compress and (T >= args.learn_start)):
-          loss, regularization_norm, weighted_regularization_norm = dqn.learn(mem, args.add_appendix, args.compress, (T >= args.learn_start), args.type_of_norm, summary_writer, T)  # Train with n-step distributional double-Q learning
-          log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | reward: ' + str(reward) + ' | loss: ' + str(loss.detach().cpu().numpy().mean()) + ' | regularization_norm: ' + str(regularization_norm.detach().cpu().numpy()) + ' | weighted regularization_norm: ' + str(weighted_regularization_norm.detach().cpu().numpy()))
-        else:
-          loss = dqn.learn(mem, args.add_appendix, args.compress, T >= (args.learn_start), args.type_of_norm, summary_writer, T)  # Train with n-step distributional double-Q learning
-          log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | reward: ' + str(reward) + ' | loss: ' + str(loss.detach().cpu().numpy().mean()))
+        dqn.learn(mem)  # Train with n-step distributional double-Q learning
 
       if T % args.evaluation_interval == 0:
         dqn.eval()  # Set DQN (online network) to evaluation mode
-        avg_reward, avg_Q = test(args, T, dqn, val_mem, args.title + '_compressed')  # Test
-        summary_writer.add_scalar('avg_reward', avg_reward, T)
-        previous_forward = dqn.online_net.forward
-        dqn.online_net.forward = dqn.online_net.regular_forward
-        uncompressed_avg_reward, uncompressed_avg_Q = test(args, T, dqn, val_mem, args.title + '_not_compressed')  # Test
-        summary_writer.add_scalar('uncompressed_avg_reward', uncompressed_avg_reward, T)
-        dqn.online_net.forward = previous_forward
-
+        avg_reward, avg_Q = test(args, T, dqn, val_mem)  # Test
         log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
         dqn.train()  # Set DQN (online network) back to training mode
 
@@ -160,5 +131,3 @@ else:
     state = next_state
 
 env.close()
-summary_writer.export_scalars_to_json("./{}_all_scalars.json".format(args.title))
-summary_writer.close()
